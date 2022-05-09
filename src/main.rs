d836e0b9c14822ea088974959a958ca3d7cbc378
Asger Hautop Drewsen <asgerdrewsen@gmail.com>
2022-05-09 14:31:39 +0200

    Use tokio

--- src/main.rs
+++ src/main.rs
@@ -1,71 +1,70 @@
 use openssl::ssl::{SslConnector, SslMethod, SslVerifyMode};
-use std::io::{ErrorKind, Read, Write};
-use std::net::{TcpListener, TcpStream};
-use std::thread;
-use std::time::Duration;
+use std::io;
+use std::pin::Pin;
 use structopt::StructOpt;
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::net::{TcpListener, TcpStream};
+use tokio_openssl::SslStream;
 
-fn copy_buf<R: Read, W: Write>(
+async fn handle_read<W: AsyncWriteExt + std::marker::Unpin>(
+    opt: &Opt,
     i: usize,
     arrow: &str,
-    src: &mut R,
+    read_res: io::Result<usize>,
+    buf: &[u8],
     dst: &mut W,
-    buf: &mut [u8],
-) -> Option<usize> {
-    let n = match src.read(buf) {
-        Ok(n) => Some(n),
-        Err(e) => match e.kind() {
-            ErrorKind::WouldBlock => None,
-            _ => panic!("Error: {:?}", e),
-        },
-    };
-    if let Some(n) = n {
-        if n > 0 {
-            println!("[{}] {} {} bytes", i, arrow, n);
-            println!("[{}] {}", i, String::from_utf8_lossy(&buf[..n]));
-            dst.write_all(&buf[..n]).unwrap();
+) -> io::Result<usize> {
+    let n = read_res?;
+    if n > 0 {
+        let data_read = &buf[..n];
+        println!("[{}] {} {} bytes", i, arrow, n);
+        if opt.data {
+            println!("{}", String::from_utf8_lossy(data_read));
         }
+        dst.write_all(data_read).await?;
     };
-    n
+
+    Ok(n)
 }
 
-fn handle_client(opt: &Opt, i: usize, mut incoming_stream: TcpStream) {
+async fn handle_client(opt: &Opt, i: usize, mut incoming_stream: TcpStream) -> io::Result<()> {
     println!("[{}] === Handling connection ===", i);
     println!();
 
     let mut connector_builder = SslConnector::builder(SslMethod::tls()).unwrap();
     connector_builder.set_verify(SslVerifyMode::NONE);
-    let connector = connector_builder.build();
-
-    let stream = TcpStream::connect((&*opt.hostname, opt.host_port)).unwrap();
-    stream
-        .set_read_timeout(Some(Duration::from_millis(100)))
+    let ssl = connector_builder
+        .build()
+        .configure()
+        .unwrap()
+        .into_ssl(&*opt.hostname)
         .unwrap();
 
-    let mut stream = connector.connect(&opt.hostname, stream).unwrap();
+    let outgoing_stream = TcpStream::connect((&*opt.hostname, opt.host_port)).await?;
 
-    incoming_stream
-        .set_read_timeout(Some(Duration::from_millis(100)))
-        .unwrap();
+    let mut outgoing_stream = SslStream::new(ssl, outgoing_stream).unwrap();
+    Pin::new(&mut outgoing_stream).connect().await.unwrap();
 
-    let mut buf = vec![0; 1024 * 1024];
+    let mut incoming_buf = vec![0; 1024 * 1024];
+    let mut outgoing_buf = vec![0; 1024 * 1024];
     loop {
-        let n1 = copy_buf(i, "==>", &mut incoming_stream, &mut stream, &mut buf);
-        let n2 = copy_buf(i, "<==", &mut stream, &mut incoming_stream, &mut buf);
-
-        //println!("n1={:?}, n2={:?}", n1, n2);
-
-        if n1 == Some(0) && n2 == Some(0) {
-            break;
-        }
-
-        if n1 == None && n2 == Some(0) {
-            break;
-        }
+        tokio::select! {
+            n = incoming_stream.read(&mut incoming_buf) => {
+                handle_read(opt, i, "==>", n, &incoming_buf, &mut outgoing_stream).await?;
+            },
+            res = outgoing_stream.read(&mut outgoing_buf) => {
+                let n = handle_read(opt, i, "<==", res, &outgoing_buf, &mut incoming_stream).await?;
+                if n == 0 {
+                    break;
+                }
+            },
+        };
     }
 
     println!();
     println!("[{}] === Done ===", i);
+
+    Ok(())
 }
 
 #[derive(StructOpt, Clone)]
@@ -77,21 +76,27 @@ struct Opt {
 
     #[structopt(long, default_value = "443")]
     host_port: u16,
+
+    #[structopt(long)]
+    data: bool,
 }
 
-fn main() {
+#[tokio::main]
+async fn main() -> io::Result<()> {
     let opt = Opt::from_args();
 
     let ip_str = "0.0.0.0";
-    let listener = TcpListener::bind((ip_str, opt.listen_port)).unwrap();
+    let listener = TcpListener::bind((ip_str, opt.listen_port)).await?;
 
     println!("Listening on {}:{}", ip_str, opt.listen_port);
 
-    for (i, stream) in listener.incoming().enumerate() {
-        let stream = stream.unwrap();
+    let mut i: usize = usize::MAX;
+    loop {
+        i = i.wrapping_add(1);
+        let (socket, _) = listener.accept().await?;
         let opt = opt.clone();
-        thread::spawn(move || {
-            handle_client(&opt, i, stream);
+        tokio::spawn(async move {
+            handle_client(&opt, i, socket).await.unwrap();
         });
     }
 }
